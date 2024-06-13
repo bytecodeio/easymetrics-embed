4d7ffbe225b740aa95efa8fec1bf688e4ad2df0b
Colin Roy-Ehri <39524338+colin-roy-ehri@users.noreply.github.com>
2024-06-13 12:06:23 -0600

    Delete client/node_modules directory

--- client/node_modules/@visx/xychart/esm/context/EventEmitterContext.js
+++
@@ -1,3 +0,0 @@
-import React, { createContext } from 'react';
-var EventEmitterContext = createContext(null);
-export default EventEmitterContext;
--- client/node_modules/@visx/xychart/esm/context/TooltipContext.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { createContext } from 'react';
-var TooltipContext = createContext(null);
-export default TooltipContext;
